18909acca1a99d71b8cc7706c05b70f14060adba
John Pearce <John.Pearce@calbaptist.edu>
2015-09-18 00:19:28 -0700

    12

---
+++ app/src/main/java/com/pearce/john/assm1/user.java
@@ -0,0 +1,13 @@
+package com.pearce.john.assm1;
+
+/**
+ * Created by John on 9/18/2015.
+ */
+public class user {
+
+    int number;
+
+    public user(int number) {
+        this.number = number;
+    }
+}
